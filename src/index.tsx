897cbc33150ec7260a33ec85c092823029efaab3
yunho <kimyouknow@naver.com>
2022-06-13 21:16:03 +0900

    Build: msw 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 
 import App from '@/App';
-import { StrictMode } from 'react';
 import { theme } from '@/styles/theme';
 import GlobalStyle from '@/styles/GlobalStyles';
+import { worker } from '@/mocks/browser';
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
+
+if (process.env.REACT_APP_MOCK_TOOL === 'msw') {
+  worker.start();
+}
 
 root.render(
   <StrictMode>
