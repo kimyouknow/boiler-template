6f5bdf492a98af1d4602922d73beaaa31e28c0f0
yunho <kimyouknow@naver.com>
2022-06-13 14:19:43 +0900

    Build: styled-component, globalTheme, nomorlize 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from '@/App';
+import { StrictMode } from 'react';
+import { theme } from '@/styles/theme';
+import GlobalStyle from '@/styles/GlobalStyles';
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+);
