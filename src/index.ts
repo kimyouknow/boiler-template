5b6d53b81b60901a7f14b8823674e504a844db86
yunho <kimyouknow@naver.com>
2022-07-29 11:18:06 +0900

    Feat: webpack with ts-loader

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log(1);
+
+const $root = document.querySelector('#root');
+
+if ($root) {
+  $root.innerHTML = `<h1>123123</h1>`;
+}
---
+++ webpack/webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, '../public')}/index.html`,
+    }),
+  ],
+};
